d95d352aed03dfa4cf635ca08bec44dd82fc8aad
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-01 18:06:23 +0100

    Add project files.

---
+++ enum_emp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_emp
+{
+    class Program
+    {
+        enum emp : int
+        {
+            John,
+            Maria,
+            Mary = 5,
+            Peter,
+            Jack = 10,
+            Zoe
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write((int)emp.Maria + ", ");
+            Console.Write((int)emp.Peter + ", ");
+            Console.Write((int)emp.Jack + ", ");
+            Console.Write((int)emp.Zoe);
+            Console.ReadKey();
+        }
+    }
+}
+
